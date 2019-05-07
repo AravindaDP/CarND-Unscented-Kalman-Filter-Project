41de43e8fb8894a8de087f7c7b507a0f8b945c84
Pramuditha Aravinda <pramudithaa@zone24x7.com>
2019-05-07 18:00:04 +0530

    Python tdd (#9)
    * test: Add TestTools
    
      For testing CalculateRMSE function.
    
    * test: Add UKFTest
    
      For test driven development of python UKF class.

--- python/ukf/measurement_package.py
+++ python/ukf/measurement_package.py
@@ -11,3 +11,9 @@ class MeasurementPackage :
         self._timestamp =  timestamp
         self._sensor_type = sensor_type
         self._raw_measurements = raw_measurements
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
